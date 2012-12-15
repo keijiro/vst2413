3a02e225bb2ff47daac8320b5cc8fca3ea4b7c28
keijiro <keijiro@gmail.com>
2012-12-15 16:29:52 +0900

    Prefer to use STL string.

--- source/vst2413.cpp
+++ source/vst2413.cpp
@@ -1,5 +1,4 @@
 #include "vst2413.h"
-#include <cstdio>
 
 #pragma mark Creation and destruction
 
@@ -36,12 +35,12 @@ void Vst2413::setProgramName(char* name) {
 }
 
 void Vst2413::getProgramName(char* name) {
-    driver_.GetProgramName(driver_.GetProgram()).copy(name, kVstMaxProgNameLen, 0);
+    driver_.GetProgramName(driver_.GetProgram()).copy(name, kVstMaxProgNameLen);
 }
 
 bool Vst2413::getProgramNameIndexed(VstInt32 category, VstInt32 index, char* text) {
     if (index < Driver::kProgramMax) {
-        driver_.GetProgramName(static_cast<Driver::ProgramID>(index)).copy(text, kVstMaxProgNameLen, 0);
+        driver_.GetProgramName(static_cast<Driver::ProgramID>(index)).copy(text, kVstMaxProgNameLen);
         return true;
     } else {
         return false;
@@ -60,15 +59,15 @@ float Vst2413::getParameter(VstInt32 index) {
 }
 
 void Vst2413::getParameterLabel(VstInt32 index, char* text) {
-    driver_.GetParameterLabel(static_cast<Driver::ParameterID>(index)).copy(text, kVstMaxParamStrLen, 0);
+    driver_.GetParameterLabel(static_cast<Driver::ParameterID>(index)).copy(text, kVstMaxParamStrLen);
 }
 
 void Vst2413::getParameterDisplay(VstInt32 index, char* text) {
-    driver_.GetParameterText(static_cast<Driver::ParameterID>(index)).copy(text, kVstMaxParamStrLen, 0);
+    driver_.GetParameterText(static_cast<Driver::ParameterID>(index)).copy(text, kVstMaxParamStrLen);
 }
 
 void Vst2413::getParameterName(VstInt32 index, char* text) {
-    driver_.GetParameterName(static_cast<Driver::ParameterID>(index)).copy(text, kVstMaxParamStrLen, 0);
+    driver_.GetParameterName(static_cast<Driver::ParameterID>(index)).copy(text, kVstMaxParamStrLen);
 }
 
 #pragma mark
@@ -85,7 +84,7 @@ void Vst2413::setBlockSize(VstInt32 blockSize) {
 
 bool Vst2413::getOutputProperties(VstInt32 index, VstPinProperties* properties) {
     if (index == 0) {
-        strncpy(properties->label, "1 Out", kVstMaxLabelLen);
+        Driver::String("1 Out").copy(properties->label, kVstMaxLabelLen);
         properties->flags = kVstPinIsActive;
         return true;
     }
@@ -96,17 +95,17 @@ bool Vst2413::getOutputProperties(VstInt32 index, VstPinProperties* properties) 
 #pragma mark Plug-in properties
 
 bool Vst2413::getEffectName(char* name) {
-    strncpy(name, "VST2413", kVstMaxProductStrLen);
+    Driver::String("VST2413").copy(name, kVstMaxEffectNameLen);
     return true;
 }
 
 bool Vst2413::getVendorString(char* text) {
-    strncpy(text, "RadiumSoftware", kVstMaxVendorStrLen);
+    Driver::String("RadiumSoftware").copy(text, kVstMaxVendorStrLen);
     return true;
 }
 
 bool Vst2413::getProductString(char* text) {
-    strncpy(text, "VST2413", kVstMaxProductStrLen);
+    Driver::String("VST2413").copy(text, kVstMaxProductStrLen);
     return true;
 }
 
@@ -114,10 +113,11 @@ VstInt32 Vst2413::getVendorVersion() {
     return 1000;
 }
 
-VstInt32 Vst2413::canDo(char *text) {
-    if(!strcmp(text, "receiveVstEvents")) return 1;
-    if(!strcmp(text, "receiveVstMidiEvent")) return 1;
-    if(!strcmp(text, "midiProgramNames")) return 1;
+VstInt32 Vst2413::canDo(char* text) {
+    Driver::String str = text;
+    if (str == "receiveVstEvents") return 1;
+    if (str == "receiveVstMidiEvent") return 1;
+    if (str == "midiProgramNames") return 1;
     return 0;
 }
 
@@ -135,15 +135,15 @@ VstInt32 Vst2413::getNumMidiOutputChannels() {
 #pragma mark
 #pragma mark MIDI program
 
-VstInt32 Vst2413::getMidiProgramName(VstInt32 channel, MidiProgramName *mpn) {
+VstInt32 Vst2413::getMidiProgramName(VstInt32 channel, MidiProgramName* mpn) {
     return 0;
 }
 
-VstInt32 Vst2413::getCurrentMidiProgram(VstInt32 channel, MidiProgramName *mpn) {
+VstInt32 Vst2413::getCurrentMidiProgram(VstInt32 channel, MidiProgramName* mpn) {
     return 0;
 }
 
-VstInt32 Vst2413::getMidiProgramCategory(VstInt32 channel, MidiProgramCategory *category) {
+VstInt32 Vst2413::getMidiProgramCategory(VstInt32 channel, MidiProgramCategory* category) {
     return 0;
 }
 
@@ -151,7 +151,7 @@ bool Vst2413::hasMidiProgramsChanged(VstInt32 channel) {
     return false;
 }
 
-bool Vst2413::getMidiKeyName(VstInt32 channel, MidiKeyName *key) {
+bool Vst2413::getMidiKeyName(VstInt32 channel, MidiKeyName* key) {
 	key->keyName[0] = 0;
 	key->reserved = 0;
 	key->flags = 0;
