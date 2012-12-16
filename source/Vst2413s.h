b5be6829c9a6325211a0c49e6769b82b16a23bc6
keijiro <keijiro@gmail.com>
2012-12-16 20:25:12 +0900

    Cosmetic changes.

--- source/Vst2413s.h
+++ source/Vst2413s.h
@@ -11,33 +11,33 @@ public:
     Vst2413s(audioMasterCallback audioMaster);
     ~Vst2413s();
 
-	virtual void processReplacing (float** inputs, float** outputs, VstInt32 sampleFrames);
-	virtual VstInt32 processEvents (VstEvents* events);
+	virtual void processReplacing(float** inputs, float** outputs, VstInt32 sampleFrames);
+	virtual VstInt32 processEvents(VstEvents* events);
     
-	virtual void setProgram (VstInt32 program);
-	virtual void setProgramName (char* name);
-	virtual void getProgramName (char* name);
-	virtual bool getProgramNameIndexed (VstInt32 category, VstInt32 index, char* text);
+	virtual void setProgram(VstInt32 program);
+	virtual void setProgramName(char* name);
+	virtual void getProgramName(char* name);
+	virtual bool getProgramNameIndexed(VstInt32 category, VstInt32 index, char* text);
     
-	virtual void setParameter (VstInt32 index, float value);
-	virtual float getParameter (VstInt32 index);
-	virtual void getParameterLabel (VstInt32 index, char* label);
-	virtual void getParameterDisplay (VstInt32 index, char* text);
-	virtual void getParameterName (VstInt32 index, char* text);
+	virtual void setParameter(VstInt32 index, float value);
+	virtual float getParameter(VstInt32 index);
+	virtual void getParameterLabel(VstInt32 index, char* label);
+	virtual void getParameterDisplay(VstInt32 index, char* text);
+	virtual void getParameterName(VstInt32 index, char* text);
 	
-	virtual void setSampleRate (float sampleRate);
-	virtual void setBlockSize (VstInt32 blockSize);
+	virtual void setSampleRate(float sampleRate);
+	virtual void setBlockSize(VstInt32 blockSize);
 	
-	virtual bool getOutputProperties (VstInt32 index, VstPinProperties* properties);
+	virtual bool getOutputProperties(VstInt32 index, VstPinProperties* properties);
     
-	virtual bool getEffectName (char* name);
-	virtual bool getVendorString (char* text);
-	virtual bool getProductString (char* text);
-	virtual VstInt32 getVendorVersion ();
-	virtual VstInt32 canDo (char* text);
+	virtual bool getEffectName(char* name);
+	virtual bool getVendorString(char* text);
+	virtual bool getProductString(char* text);
+	virtual VstInt32 getVendorVersion();
+	virtual VstInt32 canDo(char* text);
     
-	virtual VstInt32 getNumMidiInputChannels ();
-	virtual VstInt32 getNumMidiOutputChannels ();
+	virtual VstInt32 getNumMidiInputChannels();
+	virtual VstInt32 getNumMidiOutputChannels();
 
 private:
     SynthDriver driver_;
